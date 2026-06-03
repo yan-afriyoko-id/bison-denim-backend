d4d1cdedc8cd4b3955db859f78b61d625d67db7b
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2026-06-03 09:28:52 +0700

    fix: cors url

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://karsindofurniture.co.id', 'https://cms.karsindofurniture.co.id', 'https://karsindo.vercel.app', 'https://cms-karsindo.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
